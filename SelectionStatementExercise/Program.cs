e4704c4d8aeb4dc45fe8a198b1153a9c92282ac4
Julian Armstrong <julian.armstrong8@gmail.com>
2024-08-12 13:11:24 -0600

    Completed Ex1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            //The following code will prompt the user
+            //to guess what my favorite number is.
+            Console.WriteLine("What is my favorite number? ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("nah thats too low.");
+            } else if (userInput > favNumber)
+            {
+                Console.WriteLine("nah that too high.");
+            }
+            else
+            {
+                Console.WriteLine("Nice.");
+            }
         }
     }
 }
